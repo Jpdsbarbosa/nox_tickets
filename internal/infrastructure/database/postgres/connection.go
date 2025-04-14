3e8c974c3070febb6562118c9b3b755d743b0dd4
Joao Pedro <jpdsbarbosa@inf.ufpel.edu.br>
2025-04-14 06:43:28 -0300

    feat: conexão com banco

--- internal/infrastructure/database/postgres/connection.go
+++ internal/infrastructure/database/postgres/connection.go
@@ -2,18 +2,37 @@ package postgres
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/lib/pq"
 )
 
-func NewConnection(connectionString string) (*sql.DB, error) {
-	db, err := sql.Open("postgres", connectionString)
+// config armazena as configurações de conexão com o banco
+type Config struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func NewConnection(config Config) (*sql.DB, error) {
+	// monta a string de conexão com o banco
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		config.Host, config.Port, config.User, config.Password, config.DBName, config.SSLMode,
+	)
+
+	// abre a conexão com o banco
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("erro ao abrir conexão com o banco: %v", err)
 	}
 
+	// testa a conexão com o banco
 	if err := db.Ping(); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("erro ao conectar ao banco: %v", err)
 	}
 
 	return db, nil
